52dd3e0069b8e712f31b3fddc0c9c888f7e0bf78
Anton Bevzuk <a.bevzuk@dodopizza.com>
2019-08-08 21:23:21 +0300

    Rename test file

--- Domain.Test/BacklogTests.cs
+++ Domain.Test/BacklogTests.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using Domain;
 using NUnit.Framework;
 
@@ -10,9 +9,9 @@ namespace Tests
         public void ByDefault_BacklogIsEmpty()
         {
             var backlog = new Backlog();
-            
+
             var backlogItems = backlog.Items;
-            
+
             CollectionAssert.IsEmpty(backlogItems);
         }
     }
